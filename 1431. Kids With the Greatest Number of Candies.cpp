212dddaf60b79ccf7d2a19adb40b2ee4abbaeae3
Yerkebulan <83460494+Urrazaliyev@users.noreply.github.com>
2022-11-08 16:39:44 +0600

    Create 1431. Kids With the Greatest Number of Candies.cpp

---
+++ 1431. Kids With the Greatest Number of Candies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> ans;
+        int maxi=INT_MIN;
+        for(int i=0;i<candies.size();i++){
+           if(candies[i]>maxi)maxi=candies[i];
+        }
+        for(int i=0;i<candies.size();i++){
+            if(candies[i]+extraCandies>=maxi)
+                ans.push_back(true);
+             else ans.push_back(false);   
+        }
+   return ans;
+    }
+};
